277b04b478cd5de1f4414f20ed324a3710251d80
DrNomanIslam <noman.islam@hotmail.com>
2023-08-10 21:24:14 +0500

    Run query with replacements

--- helpers/postgresQueries.js
+++ helpers/postgresQueries.js
@@ -6,6 +6,15 @@ const runQuery = async (db, query) => {
 }
 
 
+const runQueryWithReplacements = async (db, query, replacements) => {
+    let queryType = query?.split(' ')[0]?.toUpperCase()
+    let Type = (db?.QueryTypes?.[queryType]) ? (db?.QueryTypes?.[queryType]) : (db?.QueryTypes?.["SELECT"]) 
+    const data = await db.query(query, { replacements: replacements, type: Type })
+    return Array.isArray(data) ? data?.flat() : data
+}
+
+
 module.exports = {
-    runQuery
+    runQuery,
+    runQueryWithReplacements
 }
